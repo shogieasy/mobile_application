b3e2d1c588527944efd6edbba8e32b2e28ba986a
Rafał Rutkowski <65781331+shogieasy@users.noreply.github.com>
2021-11-03 02:38:08 +0100

    zad1
    zad1

---
+++ zad1/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,9 @@
+
+P
+ app/src/main/AndroidManifest.xml,8\c\8c55c3ccc257e5907959013f99656e4c8ec3903e
+h
+8app/src/main/java/com/example/zad1quiz/MainActivity.java,5\6\56db6d1e64f46614f8bbbbbf97ae82d87fd257fa
+d
+4app/src/main/java/com/example/zad1quiz/Question.java,0\c\0cc50cd699aaac3673ee90439df88c8f7f9e7120
+@
+app/build.gradle,f\4\f4a01d6a4fcb971362ec00a83903fd3902f52164
---
+++ zad1/app/src/main/java/com/example/zad1quiz/Question.java
@@ -0,0 +1,19 @@
+package com.example.zad1quiz;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Question
+{
+
+        private int questionId;
+        private boolean trueAnswer;
+
+        public Question(int questionId, boolean trueAnswer)
+        {
+            this.questionId = questionId;
+            this.trueAnswer = trueAnswer;
+        }
+
+}
